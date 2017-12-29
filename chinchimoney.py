e05e837a665e0996b11e360b7db58d5e30083914
Hermosindo <sergiogalego13@gmail.com>
2017-12-29 02:48:42 +0100

    changed variable names

--- chinchimoney.py
+++ chinchimoney.py
@@ -1,238 +1,240 @@
 from tkinter import *
 import random
 
-def addCoin():
-	if (userCoin.get()<3):
-		userCoin.set(userCoin.get()+1)
-	setCoin()
-def subtractCoin():
+def add_coin():
+	if (user_coin.get()<3):
+		user_coin.set(user_coin.get()+1)
+	set_coin()
+def subtract_coin():
 	if (match.get()==1):	
-		if (userCoin.get()>1):
-			userCoin.set(userCoin.get()-1)
+		if (user_coin.get()>1):
+			user_coin.set(user_coin.get()-1)
 	else:
-		if (userCoin.get()>0):
-			userCoin.set(userCoin.get()-1)
+		if (user_coin.get()>0):
+			user_coin.set(user_coin.get()-1)
 
-	setCoin()
-def setCoin():
-	if(userCoin.get()==0):
+	set_coin()
+def set_coin():
+	if(user_coin.get()==0):
 		image_label.img = PhotoImage(file="user0.gif")
 		image_label.config(image=image_label.img)
-	elif(userCoin.get()==1):
+	elif(user_coin.get()==1):
 		image_label.img = PhotoImage(file="user1.gif")
 		image_label.config(image=image_label.img)
-	elif(userCoin.get()==2):
+	elif(user_coin.get()==2):
 		image_label.img = PhotoImage(file="user2.gif")
 		image_label.config(image=image_label.img)
-	elif(userCoin.get()==3):
+	elif(user_coin.get()==3):
 		image_label.img = PhotoImage(file="user3.gif")
 		image_label.config(image=image_label.img)
 	image_label.pack()
 
-def setStage():
+def set_stage():
 	if(stage.get()==0):
-		buttonDOWN.config(state=NORMAL)
-		buttonUP.config(state=NORMAL)
-		buttonOK.config(text="Aceptar")
-		labelMatch.set("Partida {} \n {} - {}".format(match.get(),userWinnings.get(),computerWinnings.get()))
-		labelStage.set("Selecciona la cantidad de monedas")
+		button_down.config(state=NORMAL)
+		button_up.config(state=NORMAL)
+		button_ok.config(text="Aceptar")
+		label_match.set("Partida {} \n {} - {}".format(match.get(),user_winnings.get(),computer_winnings.get()))
+		label_stage.set("Selecciona la cantidad de monedas")
 	elif(stage.get()==1 and match.get()%2==1):
-		buttonDOWN.config(state=NORMAL)
-		buttonUP.config(state=NORMAL)
-		userChoice.set(userCoin.get())
-		setComputerChoice()
-		labelStage.set("Selecciona las monedas que cres que tiene Computer")
+		button_down.config(state=NORMAL)
+		button_up.config(state=NORMAL)
+		user_choice.set(user_coin.get())
+		set_computer_choice()
+		label_stage.set("Selecciona las monedas que cres que tiene Computer")
 	elif(stage.get()==2 and match.get()%2==1):
-		buttonDOWN.config(state=DISABLED)
-		buttonUP.config(state=DISABLED)
-		userPredict.set(userCoin.get()+userChoice.get())
-		setComputerPredict()
-		labelStage.set("Computer dice: En total hay {} monedas".format(computerPredict.get()))
+		button_down.config(state=DISABLED)
+		button_up.config(state=DISABLED)
+		user_predict.set(user_coin.get()+user_choice.get())
+		set_computer_predict()
+		label_stage.set("Computer dice: En total hay {} monedas".format(computer_predict.get()))
 	elif(stage.get()==1 and match.get()%2==0):
-		buttonDOWN.config(state=DISABLED)
-		buttonUP.config(state=DISABLED)
-		userChoice.set(userCoin.get())
-		setComputerPredict()
-		labelStage.set("Computer dice: En total hay {} monedas".format(computerPredict.get()))
+		button_down.config(state=DISABLED)
+		button_up.config(state=DISABLED)
+		user_choice.set(user_coin.get())
+		set_computer_predict()
+		label_stage.set("Computer dice: En total hay {} monedas".format(computer_predict.get()))
 	elif(stage.get()==2 and match.get()%2==0):
-		buttonDOWN.config(state=NORMAL)
-		buttonUP.config(state=NORMAL)
-		labelStage.set("Selecciona las monedas que cres que tiene Computer")
+		button_down.config(state=NORMAL)
+		button_up.config(state=NORMAL)
+		label_stage.set("Selecciona las monedas que cres que tiene Computer")
 	elif(stage.get()==3):
-		buttonDOWN.config(state=DISABLED)
-		buttonUP.config(state=DISABLED)
-		userPredict.set(userCoin.get()+userChoice.get())
-		realCoin.set(userChoice.get()+computerChoice.get())
-		showUserCoin()
-		showComputerCoin()
-		labelStage.set("Había {} monedas!".format(realCoin.get()))
+		button_down.config(state=DISABLED)
+		button_up.config(state=DISABLED)
+		user_predict.set(user_coin.get()+user_choice.get())
+		real_coin.set(user_choice.get()+computer_choice.get())
+		showuser_coin()
+		show_computer_coin()
+		label_stage.set("Había {} monedas!".format(real_coin.get()))
 	elif(stage.get()==4):
-		buttonDOWN.config(state=DISABLED)
-		buttonUP.config(state=DISABLED)
-		setWinner()
-		labelStage.set("{} ha ganado.".format(labelWinner.get()))
+		button_down.config(state=DISABLED)
+		button_up.config(state=DISABLED)
+		set_winner()
+		label_stage.set("{} ha ganado.".format(label_winner.get()))
 		image_computer.img = PhotoImage(file="hc.gif")
 		image_computer.config(image=image_computer.img)
-		labelMatch.set("Partida {} \n {} - {}".format(match.get(),userWinnings.get(),computerWinnings.get()))
-		gameOver()
+		label_match.set("Partida {} \n {} - {}".format(match.get(),user_winnings.get(),computer_winnings.get()))
+		game_over()
 		match.set(match.get()+1)
 		stage.set(-1)
 
-def addStage():
+def add_stage():
 	stage.set(stage.get()+1)
-	setStage()
-	buttonUP.pack()
-	buttonDOWN.pack()
+	set_stage()
+	button_up.pack()
+	button_down.pack()
 
-def setComputerChoice():
+def set_computer_choice():
 	if (match.get()==1):
-		computerChoice.set(random.randrange(1,4))
+		computer_choice.set(random.randrange(1,4))
 	else:
-		computerChoice.set(random.randrange(0,4))
+		computer_choice.set(random.randrange(0,4))
 
-def setComputerPredict():
+def set_computer_predict():
 	if (match.get()%2==1):
-		if (userPredict.get()==0):
-			computerPredict.set(computerChoice.get()+0)
-		elif (userPredict.get()==1):
-			computerPredict.set(computerChoice.get()+random.randrange(0,2))
-		elif (userPredict.get()==2):
-			computerPredict.set(computerChoice.get()+random.randrange(0,3))
-		elif (userPredict.get()==3):
-			computerPredict.set(computerChoice.get()+random.randrange(0,4))
-		elif (userPredict.get()==4):
-			computerPredict.set(computerChoice.get()+random.randrange(1,4))
-		elif (userPredict.get()==5):
-			computerPredict.set(computerChoice.get()+random.randrange(2,4))
-		elif (userPredict.get()==6):
-			computerPredict.set(computerChoice.get()+3)
+		if (user_predict.get()==0):
+			computer_predict.set(computer_choice.get()+0)
+		elif (user_predict.get()==1):
+			computer_predict.set(computer_choice.get()+random.randrange(0,2))
+		elif (user_predict.get()==2):
+			computer_predict.set(computer_choice.get()+random.randrange(0,3))
+		elif (user_predict.get()==3):
+			computer_predict.set(computer_choice.get()+random.randrange(0,4))
+		elif (user_predict.get()==4):
+			computer_predict.set(computer_choice.get()+random.randrange(1,4))
+		elif (user_predict.get()==5):
+			computer_predict.set(computer_choice.get()+random.randrange(2,4))
+		elif (user_predict.get()==6):
+			computer_predict.set(computer_choice.get()+3)
 	elif (match.get()%2==0):
-		computerPredict.set(computerChoice.get()+random.randrange(0,4))
+		computer_predict.set(computer_choice.get()+random.randrange(0,4))
 
-def setWinner():
-	if (userPredict.get()==realCoin.get() and userPredict.get()!=computerPredict.get()):
-		labelWinner.set("Usuario")
-		userWinnings.set(userWinnings.get()+1)
-	elif (computerPredict.get()==realCoin.get() and userPredict.get()!=computerPredict.get()):
-		labelWinner.set("Computer")
-		computerWinnings.set(computerWinnings.get()+1)
+def set_winner():
+	if (user_predict.get()==real_coin.get() and user_predict.get()!=computer_predict.get()):
+		label_winner.set("Usuario")
+		user_winnings.set(user_winnings.get()+1)
+	elif (computer_predict.get()==real_coin.get() and user_predict.get()!=computer_predict.get()):
+		label_winner.set("Computer")
+		computer_winnings.set(computer_winnings.get()+1)
 	else:
-		labelWinner.set("Nadie")
+		label_winner.set("Nadie")
 
-def showComputerCoin():
-	if(computerChoice.get()==0):
+def show_computer_coin():
+	if(computer_choice.get()==0):
 		image_computer.img = PhotoImage(file="user0.gif")
 		image_computer.config(image=image_computer.img)
-	elif(computerChoice.get()==1):
+	elif(computer_choice.get()==1):
 		image_computer.img = PhotoImage(file="user1.gif")
 		image_computer.config(image=image_computer.img)
-	elif(computerChoice.get()==2):
+	elif(computer_choice.get()==2):
 		image_computer.img = PhotoImage(file="user2.gif")
 		image_computer.config(image=image_computer.img)
-	elif(computerChoice.get()==3):
+	elif(computer_choice.get()==3):
 		image_computer.img = PhotoImage(file="user3.gif")
 		image_computer.config(image=image_computer.img)
 	image_computer.pack()
 
-def showUserCoin():
-	if(userChoice.get()==0):
+def showuser_coin():
+	if(user_choice.get()==0):
 		image_label.img = PhotoImage(file="user0.gif")
 		image_label.config(image=image_label.img)
-		userCoin.set(0)
-	elif(userChoice.get()==1):
+		user_coin.set(0)
+	elif(user_choice.get()==1):
 		image_label.img = PhotoImage(file="user1.gif")
 		image_label.config(image=image_label.img)
-		userCoin.set(1)
-	elif(userChoice.get()==2):
+		user_coin.set(1)
+	elif(user_choice.get()==2):
 		image_label.img = PhotoImage(file="user2.gif")
 		image_label.config(image=image_label.img)
-		userCoin.set(2)
-	elif(userChoice.get()==3):
+		user_coin.set(2)
+	elif(user_choice.get()==3):
 		image_label.img = PhotoImage(file="user3.gif")
 		image_label.config(image=image_label.img)
-		userCoin.set(3)
+		user_coin.set(3)
 	image_label.pack()
 
-def gameOver():
-	if (userWinnings.get()==5 or computerWinnings.get()==5):
-		buttonOK.config(text="Nueva Partida")
-		buttonOK.pack()
-		computerWinnings.set(0)
-		userWinnings.set(0)
+def game_over():
+	if (user_winnings.get()==5 or computer_winnings.get()==5):
+		button_ok.config(text="Nueva Partida")
+		button_ok.pack()
+		computer_winnings.set(0)
+		user_winnings.set(0)
 		match.set(0)
 
+
 root = Tk()
-userCoin = IntVar()
-computerCoin = IntVar()
+
+user_coin = IntVar()
 match = IntVar()
 stage = IntVar()
-userChoice = IntVar()
-userPredict = IntVar()
-computerChoice = IntVar()
-computerPredict = IntVar()
-realCoin = IntVar()
-userWinnings = IntVar()
-computerWinnings = IntVar()
-labelStage = StringVar()
-labelMatch = StringVar()
-labelWinner = StringVar()
-labelStage.set("Selecciona la cantidad de monedas")
-userWinnings.set(0)
-computerWinnings.set(0)
+user_choice = IntVar()
+user_predict = IntVar()
+computer_choice = IntVar()
+computer_predict = IntVar()
+real_coin = IntVar()
+user_winnings = IntVar()
+computer_winnings = IntVar()
+label_stage = StringVar()
+label_match = StringVar()
+label_winner = StringVar()
+
+label_stage.set("Selecciona la cantidad de monedas")
+user_winnings.set(0)
+computer_winnings.set(0)
 stage.set(0) 
 match.set(1)
-userCoin.set(1)
-labelMatch.set("Partida {} \n {} - {}".format(match.get(),userWinnings.get(),computerWinnings.get()))
+user_coin.set(1)
+label_match.set("Partida {} \n {} - {}".format(match.get(),user_winnings.get(),computer_winnings.get()))
 root.title("Chinchimoney")
 root.resizable(0,0)
 root.iconbitmap('icon.ico')
-titleImage = Frame(root)
-titleImage.grid(row=0,column=0,columnspan=2)
-titleImage.config(width= 256, height=64)
-titleImage.config(bd=5)
-scoreBoard = Frame(root)
-scoreBoard.grid(row=1,column=0,columnspan=2)
-scoreBoard.config(width= 256, height=32)
-scoreBoard.config(bd=5)
-displayText = Frame(root)
-displayText.grid(row=2,column=0,columnspan=2)
-displayText.config(width= 256, height=32)
-displayText.config(bd=3)
-userUP = Frame(root)
-userUP.grid(row=3,column=0)
-userUP.config(width= 128, height=32)
-userUP.config(bd=5)
-userDisplay = Frame(root)
-userDisplay.grid(row=4,column=0)
-userDisplay.config(width= 128, height=192)
 
-computerDisplay = Frame(root)
-computerDisplay.grid(row=4,column=1)
-computerDisplay.config(width= 128, height=192)
+title_image = Frame(root)
+title_image.grid(row=0,column=0,columnspan=2)
+title_image.config(width= 256, height=64)
+title_image.config(bd=5)
+score_board = Frame(root)
+score_board.grid(row=1,column=0,columnspan=2)
+score_board.config(width= 256, height=32)
+score_board.config(bd=5)
+display_text = Frame(root)
+display_text.grid(row=2,column=0,columnspan=2)
+display_text.config(width= 256, height=32)
+display_text.config(bd=3)
+user_up = Frame(root)
+user_up.grid(row=3,column=0)
+user_up.config(width= 128, height=32)
+user_up.config(bd=5)
+user_display = Frame(root)
+user_display.grid(row=4,column=0)
+user_display.config(width= 128, height=192)
+computer_display = Frame(root)
+computer_display.grid(row=4,column=1)
+computer_display.config(width= 128, height=192)
+user_down = Frame(root)
+user_down.grid(row=5,column=0)
+user_down.config(width= 128, height=32)
+user_down.config(bd=5)
+button_frame = Frame(root)
+button_frame.grid(row=6,column=0,columnspan=2)
+button_frame.config(width= 256, height=32)
 
-userDOWN = Frame(root)
-userDOWN.grid(row=5,column=0)
-userDOWN.config(width= 128, height=32)
-userDOWN.config(bd=5)
-buttonFrame = Frame(root)
-buttonFrame.grid(row=6,column=0,columnspan=2)
-buttonFrame.config(width= 256, height=32)
-buttonUP = Button(userUP,text="Más",command=addCoin)
-buttonDOWN = Button(userDOWN,text="Menos",command=subtractCoin)
-buttonUP.pack()
-buttonDOWN.pack()
-Label(buttonFrame).pack()
-buttonOK = Button(buttonFrame,text="Aceptar",command=addStage)
-buttonOK.pack()
-Label(buttonFrame).pack()
+button_up = Button(user_up,text="Más",command=add_coin)
+button_down = Button(user_down,text="Menos",command=subtract_coin)
+button_up.pack()
+button_down.pack()
+Label(button_frame).pack()
+button_ok = Button(button_frame,text="Aceptar",command=add_stage)
+button_ok.pack()
+Label(button_frame).pack()
 userImage = PhotoImage(file="user1.gif")
-image_label = Label(userDisplay, image=userImage)
+image_label = Label(user_display, image=userImage)
 image_label.pack()
-computerImage = PhotoImage(file="hc.gif")
-image_computer = Label(computerDisplay, image=computerImage)
+computer_image = PhotoImage(file="hc.gif")
+image_computer = Label(computer_display, image=computer_image)
 image_computer.pack()
 header = PhotoImage(file="header.gif")
-Label(titleImage, image=header).pack()
-Label(displayText, textvariable=labelStage).pack()
-Label(scoreBoard, textvariable=labelMatch).pack()
+Label(title_image, image=header).pack()
+Label(display_text, textvariable=label_stage).pack()
+Label(score_board, textvariable=label_match).pack()
 root.mainloop()
